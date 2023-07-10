b35bd0dc21955f84a4dd9bbfae39b0b3b50dcea9
richie <arnoldrichie31.rar@gmail.com>
2023-07-10 22:15:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Stylesheets from "./components/Stylesheets";
+import FunctionClick from "./components/FunctionClick";
+import Inline from "./components/inline";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className=" bg-success">
+        <Stylesheets primary={true} />
+        <Inline />
+        <FunctionClick />
       </header>
     </div>
   );
